6617f099f41f8f118043bf3876ae48d8123ee89a
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-09-30 03:46:54 +0500

    commit

--- lib/devcamp_ui.dart
+++ lib/devcamp_ui.dart
@@ -11,25 +11,37 @@ class _FlutterDevCampUIState extends State<FlutterDevCampUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-
-          Text('Welcome to DevCamp',style: TextStyle(fontSize: 25,),),
-          SizedBox(height: 20,),
+          Text(
+            'Welcome to DevCamp',
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
           Image.asset('assets/poster.jpg'),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           Center(
-            child: ElevatedButton(onPressed: (){},
-                style: ButtonStyle(
-                  backgroundColor: const WidgetStatePropertyAll<Color>(Colors.blue),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor:
+                  const WidgetStatePropertyAll<Color>(Colors.blue),
                   shape: const WidgetStatePropertyAll<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius:BorderRadius.all(Radius.circular(12)),
-                    )
-                  )
-                  ), child: Text('Click Me',style: TextStyle(color: Colors.white),),
-                ),
-                
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ))),
+              child: Text(
+                'Click Me',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           )
         ],
       ),
