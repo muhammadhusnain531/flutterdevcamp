f904b4fad14f5944bc3b684d5a39ea5437aef662
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-09-30 03:46:01 +0500

    commit

--- lib/devcamp_ui.dart
+++ lib/devcamp_ui.dart
@@ -16,7 +16,21 @@ class _FlutterDevCampUIState extends State<FlutterDevCampUI> {
 
           Text('Welcome to DevCamp',style: TextStyle(fontSize: 25,),),
           SizedBox(height: 20,),
-          Image.asset('assets/poster.jpg')
+          Image.asset('assets/poster.jpg'),
+          SizedBox(height: 20,),
+          Center(
+            child: ElevatedButton(onPressed: (){},
+                style: ButtonStyle(
+                  backgroundColor: const WidgetStatePropertyAll<Color>(Colors.blue),
+                  shape: const WidgetStatePropertyAll<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius:BorderRadius.all(Radius.circular(12)),
+                    )
+                  )
+                  ), child: Text('Click Me',style: TextStyle(color: Colors.white),),
+                ),
+                
+          )
         ],
       ),
     );
