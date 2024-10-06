1a419abb86226a56d325b9f9e260b3d32bdb180d
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2024-10-07 02:37:51 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class Myapp extends StatelessWidget {
           fontWeight: FontWeight.bold),),
       
     ),
-        body: FlutterDevCampUI(),
+        body: splash_scr(),
         )
     );  }
 }
